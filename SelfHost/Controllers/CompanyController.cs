c2e3f2f8a01dad35f57c646a0ac6dc672041a770
gideon0701 <gideon.050196@gmail.com>
2018-08-29 16:44:58 +0800

    adasd

--- SelfHost/Controllers/CompanyController.cs
+++ SelfHost/Controllers/CompanyController.cs
@@ -20,12 +20,12 @@
             companyModel.HeadlinesSentiment = new List<string>();
             companyModel.Headlines = new List<string>();
 
-            var strNewsUrl = "https://www.reuters.com/";
-            var strQuery = "search/news?blob=";
+            var strNewsUrl = "https://www.reuters.com";
+            var strQuery = "/search/news?blob=";
             var strClientName = name;
 
             var strUrl = strNewsUrl + strQuery + strClientName;
-            HtmlWeb web = new HtmlWeb();
+            HtmlWeb web = new HtmlWeb();    
             var htmlDoc = web.Load(strUrl);
 
             var xpath = "//*[self::h1 or self::h2 or self::h3 or self::h4]";
@@ -37,11 +37,13 @@
             foreach (var res in results)
             {
                 var headLines = res.InnerText;
-                Console.WriteLine(new Sentence(headLines).Sentiment);
+                var link = res.Attributes["href"].Value;
+                link = link.Contains("http") ? link : strNewsUrl + link;
+
                 companyModel.Headlines.Add(headLines);
+                companyModel.Links.Add(link);
                 companyModel.HeadlinesSentiment.Add(new Sentence(headLines).Sentiment.ToString());
             }
-
 
             companyModel.RiskScore = 123;
             return companyModel;
--- SelfHost/Utils/DownloadFileUtil.cs
+++ SelfHost/Utils/DownloadFileUtil.cs
@@ -17,5 +17,6 @@ namespace SelfHost.Utils
 
             }
         }
+
     }
 }
