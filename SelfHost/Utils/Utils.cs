d5a5246c02c17c69b7c24ed0cb621eca2b7a3082
carlodizon03 <carlodizon03@gmail.com>
2018-09-02 01:56:37 +0800

    Carlo 1:56 AM

--- SelfHost/Utils/Utils.cs
+++ SelfHost/Utils/Utils.cs
@@ -5,6 +5,8 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using edu.stanford.nlp.ie.crf;
 
 namespace SelfHost
 {
@@ -30,6 +32,66 @@ namespace SelfHost
                 }
             }
             throw new Exception("No network adapters with an IPv4 address in the system!");
+        }
+        public static Dictionary<string,string> ner(string content)
+        {
+
+
+            Dictionary<string, string> EntityDict = new Dictionary<string, string>();
+            // Path to the folder with classifiers models
+            var jarRoot = @"C:\Users\CarloDp\Documents\NLP\NER\stanford-ner-2017-06-09";
+            var classifiersDirecrory = jarRoot + @"\classifiers";
+            // Loading 7 class classifier model
+            var classifier = CRFClassifier.getClassifierNoExceptions(
+                classifiersDirecrory + @"\english.muc.7class.distsim.crf.ser.gz");
+            // Applying ner tagging and saving to xml string
+            var ner = classifier.classifyWithInlineXML(content);
+            // Adding root  
+            ner = "<root>" + ner + "</root>";
+            // Converting to Xml document
+            System.Xml.XmlDocument xmlDoc = new System.Xml.XmlDocument();
+            xmlDoc.LoadXml(ner);
+            // Iterating inside Xml Document
+            foreach (XmlNode item in xmlDoc.ChildNodes[0].ChildNodes)
+            {
+                var entity = item.Name;
+
+                if (entity == "ORGANIZATION")
+                {
+                    EntityDict.Add(item.InnerText, entity);
+                }
+                if (entity == "LOCATION")
+                {
+                    EntityDict.Add(item.InnerText, entity);
+
+                }
+                if (entity == "PERSON")
+                {
+                    EntityDict.Add(item.InnerText, entity);
+
+                }
+                if (entity == "MONEY")
+                {
+                    EntityDict.Add(item.InnerText, entity);
+
+                }
+                if (entity == "DATE")
+                {
+                    EntityDict.Add(item.InnerText, entity);
+                }
+                if (entity == "PERCENT")
+                {
+                    EntityDict.Add(item.InnerText, entity);
+                }
+                if (entity == "TIME")
+                {
+                    EntityDict.Add(item.InnerText, entity);
+
+                }
+            }
+
+          
+            return EntityDict;
         }
 
     }
